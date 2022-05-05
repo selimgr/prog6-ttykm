38b7303b397357d94e6301bdefff8dafb8e9eb38
dcs-b <81160533+dcs-b@users.noreply.github.com>
2022-05-05 17:06:29 +0200

    Squelette du paquetage Global

---
+++ src/Global/Configuration.java
@@ -0,0 +1,31 @@
+package Global;
+
+import java.io.InputStream;
+import java.util.Properties;
+import java.util.logging.Logger;
+
+public class Configuration {
+    private static Configuration instance;
+    Properties p;
+    Logger l;
+
+    public static InputStream chargerFichier(String nomFichier) {
+        return null;
+    }
+
+    private Configuration() {
+
+    }
+
+    public static Configuration instance() {
+        return null;
+    }
+
+    public String lirePropriete(String nomPropriete) {
+        return null;
+    }
+
+    public Logger logger() {
+        return null;
+    }
+}

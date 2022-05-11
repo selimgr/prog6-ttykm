1405c8e0ac121827d46f713090fc22f36df5287e
selimgr <jaawis.dev@gmail.com>
2022-05-11 11:02:25 +0200

    fenêtre jeu modifiée - version non finale

--- src/Vue/VueDemarrage.java
+++ src/Vue/VueDemarrage.java
@@ -6,7 +6,12 @@ import java.awt.*;
 class VueDemarrage extends JPanel {
 
     VueDemarrage(CollecteurEvenements c) {
-        setBackground(Color.BLUE);
+        setBackground(Color.MAGENTA);
+        setFont(new Font("Comic Sans MS", Font.BOLD, 18));
+
+        JLabel texte = new JLabel("bonjour bienvenu sur leu jeu");
+        texte.setFont(new Font("Comic Sans MS", Font.BOLD, 24));
+        add(texte);
 
         JButton boutonMenu = new JButton("Menu Principal");
         boutonMenu.addActionListener(new EcouteurMenuPrincipal(c));

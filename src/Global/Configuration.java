b878501dd40b3d88c42d235d87add228fb5b653a
dcs-b <81160533+dcs-b@users.noreply.github.com>
2022-05-24 14:15:37 +0200

    Implémentation partielle de Configuration

--- src/Global/Configuration.java
+++ src/Global/Configuration.java
@@ -1,7 +1,11 @@
 package Global;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.UncheckedIOException;
+import java.util.NoSuchElementException;
 import java.util.Properties;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Configuration {
@@ -10,19 +14,40 @@ public class Configuration {
     Logger l;
 
     public static InputStream chargerFichier(String nomFichier) {
-        return null;
+        InputStream in = ClassLoader.getSystemResourceAsStream(nomFichier);
+
+        if (in == null || nomFichier.compareTo("") == 0) {
+            throw new UncheckedIOException(new IOException("Impossible de charger le fichier " + nomFichier));
+        }
+        return in;
     }
 
     private Configuration() {
+        p = new Properties();
 
+        InputStream in = chargerFichier("default.cfg");
+
+        try {
+            p.load(in);
+        } catch (IOException e) {
+            throw new UncheckedIOException(new IOException("Impossible de charger le fichier default.cfg : ", e));
+        }
     }
 
     public static Configuration instance() {
-        return null;
+        if (instance == null) {
+            instance = new Configuration();
+        }
+        return instance;
     }
 
     public String lirePropriete(String nomPropriete) {
-        return null;
+        String valeur = p.getProperty(nomPropriete);
+
+        if (valeur == null) {
+            throw new NoSuchElementException("Propriété inexistante : " + nomPropriete);
+        }
+        return valeur;
     }
 
     public void ecrirePropriete(String nomPropriete, String valeurPropriete) {
@@ -30,6 +55,15 @@ public class Configuration {
     }
 
     public Logger logger() {
-        return null;
+        if (l == null) {
+            System.setProperty("java.util.logging.SimpleFormatter.format", "%4$s : %5$s%n");
+            l = Logger.getLogger("Gaufre.Logger");
+            l.setLevel(Level.parse(lirePropriete("LogLevel")));
+        }
+        return l;
+    }
+
+    public void sauvegarder() {
+
     }
 }

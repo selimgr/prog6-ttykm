0c5c45597df485d05a3bed720015ae400c09743c
Selim <jaawis.dev@gmail.com>
2022-05-25 22:05:57 +0200

    refonte de l'UI complète: + optimisations + ajout texte jeu + ajout de l'affichage des graines avec actions + thème Swing par défaut

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,11 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        /*try {
-            UIManager.setLookAndFeel("com.sun.java.swing.plaf.motif.MotifLookAndFeel");
-        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) {
-            e.printStackTrace();
-        }*/
+        try {
+            UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
+        } catch (Exception e) {
+            // If Nimbus is not available, you can set the GUI to another look and feel.
+        }
         CollecteurEvenements controleur = new ControleurMediateur();
         InterfaceGraphique.demarrer(controleur);
     }

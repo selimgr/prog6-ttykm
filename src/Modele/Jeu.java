a1d9d1708b8509e6591a22a7d09df89001e17e9a
Selim <jaawis.dev@gmail.com>
2022-05-30 13:07:28 +0200

    petit fix setPions

--- src/Modele/Jeu.java
+++ src/Modele/Jeu.java
@@ -53,14 +53,15 @@ public class Jeu extends Observable {
         // Sinon le perdant de la partie précédente commence
         if (joueurActuel == -1) {
             joueurActuel = choixJoueurDebut == -1 ? rand.nextInt(2) : choixJoueurDebut;
-            // Par défaut le joueur qui commence a les pions blancs
-            joueurActuel().setPions(Pion.BLANC);
-            joueurSuivant().setPions(Pion.NOIR);
         } else if (vainqueur() == joueur1) {
             joueurActuel = 1;
         } else {
             joueurActuel = 0;
         }
+        // Par défaut le joueur qui commence a les pions blancs
+        joueurActuel().setPions(Pion.BLANC);
+        joueurSuivant().setPions(Pion.NOIR);
+
         joueur1.initialiserJoueur();
         joueur2.initialiserJoueur();
         plateau = new Plateau();

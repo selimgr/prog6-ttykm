95360c4dd9e71fac2ca92224c456cd5a5c12f629
tom7438 <tom192010@live.fr>
2022-05-31 12:30:19 +0200

    Modif visu MenuChargerPartie

--- src/Vue/VueMenuParties.java
+++ src/Vue/VueMenuParties.java
@@ -10,12 +10,12 @@ import java.awt.event.MouseEvent;
 public class VueMenuParties extends JPanel {
 
     CollecteurEvenements controleur;
-    private JRadioButton partieEssai0405Radio;
-    private JRadioButton partieEssai204Radio;
     private JButton chargerPartie;
     private JButton supprimerPartie;
     private JButton menuPrincipalButton;
     private JPanel MenuParties;
+    private JScrollPane Allparties;
+    private JList list1;
     Image t;
 
     public VueMenuParties(CollecteurEvenements c) {
@@ -45,12 +45,6 @@ public class VueMenuParties extends JPanel {
     private void $$$setupUI$$$() {
         createUIComponents();
         MenuParties.setLayout(new com.intellij.uiDesigner.core.GridLayoutManager(4, 1, new Insets(0, 0, 0, 0), -1, -1));
-        partieEssai0405Radio = new JRadioButton();
-        partieEssai0405Radio.setText("Partie \"Essai\" 04/05/2022   Joueur1 : \"David\", Joueur2 : \"Tom\"");
-        MenuParties.add(partieEssai0405Radio, new com.intellij.uiDesigner.core.GridConstraints(0, 0, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_WEST, com.intellij.uiDesigner.core.GridConstraints.FILL_NONE, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
-        partieEssai204Radio = new JRadioButton();
-        partieEssai204Radio.setText("Partie \"Essai 2\" 04/04/2022   Joueur1 : \"Léonard\", Joueur2 : \"Victoria\"");
-        MenuParties.add(partieEssai204Radio, new com.intellij.uiDesigner.core.GridConstraints(1, 0, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_WEST, com.intellij.uiDesigner.core.GridConstraints.FILL_NONE, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
         final JPanel panel1 = new JPanel();
         panel1.setLayout(new com.intellij.uiDesigner.core.GridLayoutManager(1, 2, new Insets(0, 0, 0, 0), -1, -1));
         MenuParties.add(panel1, new com.intellij.uiDesigner.core.GridConstraints(2, 0, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_NONE, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW, null, new Dimension(304, 143), null, 0, false));
@@ -75,10 +69,6 @@ public class VueMenuParties extends JPanel {
         menuPrincipalButton.setBackground(new Color(-3949375));
         menuPrincipalButton.setText("Menu Principal");
         panel2.add(menuPrincipalButton, new com.intellij.uiDesigner.core.GridConstraints(0, 0, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_NONE, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
-        ButtonGroup buttonGroup;
-        buttonGroup = new ButtonGroup();
-        buttonGroup.add(partieEssai204Radio);
-        buttonGroup.add(partieEssai0405Radio);
     }
 
     /**

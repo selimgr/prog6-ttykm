cf68d78b3acddf2d5b849f25ee9b49cd38004d5d
Selim <jaawis.dev@gmail.com>
2022-06-01 01:36:38 +0200

    nouveau menu démarrage

--- src/Vue/VueDemarrage.java
+++ src/Vue/VueDemarrage.java
@@ -1,6 +1,7 @@
 package Vue;
 
 import javax.swing.*;
+import javax.swing.plaf.basic.BasicProgressBarUI;
 import java.awt.*;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
@@ -15,49 +16,35 @@ class VueDemarrage extends JPanel {
     VueDemarrage() {
         setBackground(Color.PINK);
         setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
-        setBorder(BorderFactory.createEmptyBorder(50, 0, 0, 0));
+        setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 0));
 
         // Chargement des assets
-        t = Imager.getImageBuffer("assets/topbanner.png");
         logo = Imager.getImageBuffer("assets/logo.png");
 
-        addComponentListener(new ComponentAdapter() {
-            @Override
-            public void componentResized(ComponentEvent e) {
-                super.componentResized(e);
-                setBorder(BorderFactory.createEmptyBorder(logoHeight + 50, 0, 0, 0));
-            }
-        });
+        JProgressBar progressBar = new JProgressBar();
+        progressBar.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 0));
+        progressBar.setStringPainted(true);
+        progressBar.setString("");
+        progressBar.setForeground(new Color(255, 120, 105));
+        progressBar.setIndeterminate(true);
+
+        add(progressBar);
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        Graphics2D g2d = (Graphics2D) g;
-        g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
         int w = getWidth(), h = getHeight();
-        Color color1 = new Color(255, 140, 85);
-        Color color2 = new Color(255, 120, 105);
-        GradientPaint gp = new GradientPaint(0, 0, color1, w, h, color2);
-        g2d.setPaint(gp);
-        g2d.fillRect(0, 0, w, h);
-
-        int width = (int) (getWidth() * 1.8);
-        int height = (t.getHeight(null) * width) / t.getWidth(null);
-
-        g.drawImage(t, 0, 0, width, height, null);
+        g.setColor(new Color(14, 14, 14));
+        g.fillRect(0, 0, w, h);
         // --
 
-        width = (int) (getWidth() * 0.25);
-        logoHeight = (logo.getHeight(null) * width) / logo.getWidth(null);
+        int logoWidth = (int) (w * 0.25);
+        logoHeight = (logo.getHeight(null) * logoWidth) / logo.getWidth(null);
 
-        int x = ((getWidth() - width) / 2) - 5;
+        int x = ((w - logoWidth) / 2) - 5;
 
-        g.drawImage(logo, x, 35, width, logoHeight, null);
-        if (called == 0) {
-            setBorder(BorderFactory.createEmptyBorder(logoHeight + 50, 0, 0, 0));
-            called = 1;
-        }
+        g.drawImage(logo, x, ((h-logoHeight)/2), logoWidth, logoHeight, null);
     }
 }
